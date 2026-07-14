0c3b18b689e0ffcdf6a415bd9216d917c36ff8ea
novel <noeldsouza9481@gmail.com>
2026-07-14 18:30:14 +0530

    demo

--- src/test/java/BeforandAfter.java
+++ src/test/java/BeforandAfter.java
@@ -21,6 +21,7 @@ public class BeforandAfter {
     public void openAmazonUrl() {
         driver.get("https://www.amazon.in/");
         driver.manage().window().maximize();
+        System.out.println("hello");
     }
 
     @Test
